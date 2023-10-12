72308d43226068a1b7909161b4b01b7c737d51e1
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-10-12 21:23:35 +0800

    added mongo-demo to play around mongoDB and mongoose

---
+++ mongo-demo/index.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
+  .then(() => console.log('Connected to MongoDB....'))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
