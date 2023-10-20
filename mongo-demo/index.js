3cf0f954151c41f99f1b45626df1ac03032d36c2
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-10-20 22:25:12 +0800

    learnt CRUD in mongoose

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -16,9 +16,9 @@ const Course = mongoose.model('Course', courseSchema);
 
 const createCourse = async () => {
   const course = new Course({
-    name: 'Angular Course',
-    author: "Kelvin Chan",
-    tags: ['angular', 'frontend'],
+    name: 'ABC example Course',
+    author: "Elgar Lam",
+    tags: ['diu', 'lah'],
     isPublished: true
 });
 
@@ -54,4 +54,38 @@ const getCourses = async () => {
   console.log(courses);
 };
 
-getCourses();
+const updateCourse = async (id) => {
+  // Approach: Query first
+  // findById()
+  // Modify its properties
+  // save()
+
+  // const course = await Course.findById(id);
+  // if (!course) console.log("no course");
+
+  // course.set({
+  //   isPublished: true,
+  //   author: 'Kelvin Heyyyy'
+  // });
+
+  // const result = await course.save();
+  // console.log(result);
+
+  // Approach: Update first
+  // Update directly
+  // Optionally: get the updated document as well
+  const course = await Course.findByIdAndUpdate(id, {
+    $set: {
+      author: "Yoyoyo what's up",
+      isPublished: true
+    }
+  }, { new: true });
+  console.log(course);
+};
+
+const removeCourse = async (id) => {
+  const result = await Course.deleteOne({ _id: id });
+  console.log(result);
+};
+
+removeCourse("65328c7ea0c8a743ea65ab9d");
