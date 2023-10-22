434047fa937dfb2a29a95e1d0801fa3792483043
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-10-22 23:14:00 +0800

    learnt about custom valiadator too

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -18,7 +18,15 @@ const courseSchema = new mongoose.Schema({
     enum: ['web', 'mobile', 'network']
   },
   author: String,
-  tags: [ String ],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: " A course should have at least one tag."
+    }
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
@@ -36,11 +44,11 @@ const Course = mongoose.model('Course', courseSchema);
 const createCourse = async () => {
   const course = new Course({
     name: 'E1014',
-    category: "desktop",
+    category: "web",
     author: "hahahhaa",
-    tags: ['a', 'b'],
+    tags: ['abc'],
     isPublished: true,
-    price: 9
+    price: 10
 });
   try {
     const result = await course.save();
