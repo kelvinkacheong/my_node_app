ceaa06c589eac608748065e8a74d3ca3fd33a127
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-10-25 22:26:08 +0800

    replace all data objects in vidly_project into data in mongoDB

--- vidly_project/index.js
+++ vidly_project/index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/vidly')
+    .then(() => console.log("Connected to mongoDB...."))
+    .catch((err) => console.error(err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
