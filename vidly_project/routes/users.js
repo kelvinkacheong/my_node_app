55db27797e4687cde43fa92f30089642a329e55c
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-11-06 23:29:22 +0800

    implemented bcrypt into the process of creating user

--- vidly_project/routes/users.js
+++ vidly_project/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const lodash = require('lodash');
+const bcrypt = require('bcrypt');
 const { User, validateUser } = require('../models/user');
 
 
@@ -12,6 +13,8 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered.');
 
   user = new User(lodash.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
 
