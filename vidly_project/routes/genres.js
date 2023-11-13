ab582b39da60fbce2ed466f933dfa8e624c17220
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-11-13 23:26:50 +0800

    adding authentication to genres.js when modifying data

--- vidly_project/routes/genres.js
+++ vidly_project/routes/genres.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 const { Genre, validateGenre } = require('../models/genre');
@@ -8,7 +9,7 @@ router.get('/', async (req, res) => {
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
