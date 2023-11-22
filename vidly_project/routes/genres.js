cce6b5a6cb936c49004ca53297aa9e9102d757ff
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-11-22 23:37:31 +0800

    learnt about how to make an asyncMiddleware to handle errors, also a error middleware function, and applying them to all routes

--- vidly_project/routes/genres.js
+++ vidly_project/routes/genres.js
@@ -1,3 +1,4 @@
+const { asyncMiddleware } = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const express = require('express');
@@ -5,21 +6,21 @@ const router = express.Router();
 const { Genre, validateGenre } = require('../models/genre');
 
 
-router.get('/', async (req, res) => {
+router.get('/', asyncMiddleware(async (req, res) => {
   const genres = await Genre.find().sort({ name: 1 });
   res.send(genres);
-});
+}));
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, asyncMiddleware(async (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = new Genre({ name: req.body.name });
   await genre.save();
   res.send(genre);
-});
+}));
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', auth, asyncMiddleware(async (req, res) => {
   const { error } = validateGenre(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -28,22 +29,22 @@ router.put('/:id', auth, async (req, res) => {
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   
   res.send(genre);
-});
+}));
 
-router.delete('/:id', [auth, admin], async (req, res) => {
+router.delete('/:id', [auth, admin], asyncMiddleware(async (req, res) => {
   const genre = await Genre.findByIdAndDelete(req.params.id);
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-});
+}));
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', asyncMiddleware(async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
-});
+}));
 
 module.exports = router;
