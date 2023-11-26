928f10a0700270b24c762e12a65151c73af76bc8
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-11-27 01:31:58 +0800

    logged the error on mongoDB too

--- vidly_project/index.js
+++ vidly_project/index.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 const winston = require('winston');
+require('winston-mongodb');
 const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
@@ -15,6 +16,10 @@ const express = require('express');
 const app = express();
 
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+winston.add(new winston.transports.MongoDB({ db: "mongodb://127.0.0.1:27017/vidly", options: {
+        useUnifiedTopology: true,
+    } 
+}));
 
 if (!config.get('jwtPrivateKey')) {
     console.log('FATAL ERROR: jwtPrivateKey is not defined.');
