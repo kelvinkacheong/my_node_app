2c4ca3fc3c1657096c62f4b0e3fe310e93e10ac1
kelvinkacheong <kelvin.kacheong@gmail.com>
2023-12-04 13:23:44 +0800

    extracting all routes in index.js into a new file in startup folder

--- vidly_project/index.js
+++ vidly_project/index.js
@@ -1,19 +1,14 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals =  require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
 
 process.on('uncaughtException', (ex) => {
     winston.error(ex.message, ex);
@@ -42,16 +37,6 @@ if (!config.get('jwtPrivateKey')) {
 mongoose.connect('mongodb://127.0.0.1:27017/vidly')
     .then(() => console.log("Connected to mongoDB...."))
     .catch((err) => console.error(err));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
