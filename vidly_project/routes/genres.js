c5f25c85765fa48c2eb7337e3c0f62d3e6997592
kelvinkacheong <kelvin.kacheong@gmail.com>
2024-01-09 14:45:53 +0800

    refactoring a middleware function to validate object Id

--- vidly_project/routes/genres.js
+++ vidly_project/routes/genres.js
@@ -1,7 +1,7 @@
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const express = require('express');
-const mongoose = require('mongoose');
+const validateObjectId = require('../middleware/validateObjectId');
 const router = express.Router();
 const { Genre, validateGenre } = require('../models/genre');
 
@@ -39,10 +39,7 @@ router.delete('/:id', [auth, admin], async (req, res) => {
   res.send(genre);
 });
 
-router.get('/:id', async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    return res.status(404).send('Invalid ID');
-
+router.get('/:id', validateObjectId, async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
