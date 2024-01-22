9b3ed80a54e0d646f4de6fae39cd3e97ada64802
kelvinkacheong <kelvin.kacheong@gmail.com>
2024-01-22 17:52:34 +0800

    created an instance method to deal with the logic of setting dateReturned and rentalFee

--- vidly_project/routes/returns.js
+++ vidly_project/routes/returns.js
@@ -5,7 +5,6 @@ const auth = require('../middleware/auth');
 const validate = require('../middleware/validate');
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
-const moment = require('moment');
 
 router.post('/', [auth, validate(validateReturn)], async(req, res) => {
   const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
@@ -14,15 +13,14 @@ router.post('/', [auth, validate(validateReturn)], async(req, res) => {
 
   if (rental.dateReturned) return res.status(400).send('This return is already processed');
 
-  rental.dateReturned = new Date();
-  rental.rentalFee = moment().diff(rental.dateOut, 'days') * rental.movie.dailyRentalRate;
+  rental.return();
   await rental.save();
 
   await Movie.updateOne({ _id: rental.movie._id }, {
     $inc: { numberInStock: 1 }
   });
 
-  res.status(200).send(rental);
+  res.send(rental);
 });
 
 function validateReturn(req) {
